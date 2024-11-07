87876bdeead6724bf3fcb9fa8ba683d30cee5039
0xMastxr <0xMastxr@gmail.com>
2024-11-07 10:57:01 +0100

    Solución de errores

--- pages/api/config.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function config(_: NextApiRequest, res: NextApiResponse) {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Content-Length', '52428800');  // Aumenta el límite a 50MB
-    res.status(200).json({ message: 'Tamaño máximo configurado para las solicitudes.' });
-}
