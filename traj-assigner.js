ae592e5017f0040908770fa8ed3cccc96f5ad76e
0xMastxr <0xmastxr@gmail.com>
2025-02-20 13:13:32 +0100

    mejora gestión planes, log, traj-assigner

--- traj-assigner.js
+++ traj-assigner.js
@@ -1,12 +1,33 @@
 const { PrismaClient } = require('@prisma/client');
+const fs = require('fs');
 const prisma = new PrismaClient();
 let isProcessing = false;
+const logFile = 'log.csv';
+
+// Asegurar que el archivo CSV tiene encabezado
+if (!fs.existsSync(logFile)) {
+  fs.writeFileSync(logFile, 'id,CustomName,Inicio\n');
+}
 
 const assignNextPlan = async () => {
   if (isProcessing) return;
   isProcessing = true;
 
   try {
+    const availableMachine = await prisma.machine.findFirst({
+      where: { status: 'Disponible' },
+    });
+
+    if (!availableMachine) return;
+
+    const currentProcessingPlan = await prisma.flightPlan.findFirst({
+      where: { machineAssignedId: availableMachine.id, status: 'procesando' },
+    });
+
+    if (currentProcessingPlan) {
+      console.log(`[INFO] El plan con ID ${currentProcessingPlan.id} sigue en procesamiento.`);
+    }
+
     const nextPlan = await prisma.flightPlan.findFirst({
       where: { status: 'en cola' },
       orderBy: { createdAt: 'asc' },
@@ -14,11 +35,7 @@ const assignNextPlan = async () => {
 
     if (!nextPlan) return;
 
-    const availableMachine = await prisma.machine.findFirst({
-      where: { status: 'Disponible' },
-    });
-
-    if (!availableMachine) return;
+    const startTime = new Date().toLocaleString();
 
     await prisma.flightPlan.update({
       where: { id: nextPlan.id },
@@ -33,8 +50,10 @@ const assignNextPlan = async () => {
       data: { status: 'Ocupada' },
     });
 
-    const currentTime = new Date().toLocaleString(); // Obtiene la hora y la fecha en formato local
-    console.log(`[${currentTime}] Asignado el plan con ID ${nextPlan.id} a la máquina con ID ${availableMachine.id}`);
+    console.log(`[${startTime}] Asignado plan con ID ${nextPlan.id} a máquina ${availableMachine.id}`);
+
+    // Registrar el tiempo de inicio en el archivo CSV
+    fs.appendFileSync(logFile, `${nextPlan.id},${nextPlan.customName},${startTime}\n`);
   } catch (error) {
     console.error("Error al asignar el plan:", error);
   } finally {
@@ -43,9 +62,7 @@ const assignNextPlan = async () => {
 };
 
 const main = async () => {
-  setInterval(async () => {
-    await assignNextPlan();
-  }, 1000);
+  setInterval(assignNextPlan, 10); // Revisión cada 10ms para evitar sobrecarga
 };
 
 main().catch((e) => {
